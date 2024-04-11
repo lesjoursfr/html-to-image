21321653442021eb1d7cf5c9de00594af740df62
Adrien ERAUD <e-adrien@users.noreply.github.com>
2024-04-11 11:10:21 +0200

    Add option to use XVFB

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,17 @@ import { promisify } from "util";
 const execp = promisify(exec);
 const script = resolve(__dirname, "./electron.js");
 
-interface OperationResult {
+export enum HtmlToImageOutputType {
+  png = "png",
+  jpeg = "jpeg",
+}
+
+export interface HtmlToImageOptions {
+  xvfb: boolean;
+  xvfbArgs: string | undefined;
+}
+
+export interface OperationResult {
   error: string | undefined;
   result: "ok" | undefined;
 }
@@ -24,30 +34,43 @@ function cleanOutput(std: string): OperationResult | null {
   return result;
 }
 
-export enum HtmlToImageOutputType {
-  png = "png",
-  jpeg = "jpeg",
-}
-
 export class HtmlToImage {
   url: URL;
   output: string;
   selector: string;
   outputType: HtmlToImageOutputType;
+  options: HtmlToImageOptions;
 
-  constructor(url: URL, output: string, selector: string, outputType?: HtmlToImageOutputType) {
+  constructor(
+    url: URL,
+    output: string,
+    selector: string,
+    outputType?: HtmlToImageOutputType,
+    options?: HtmlToImageOptions
+  ) {
     // Required options
     this.url = url;
     this.output = output;
     this.selector = selector;
     this.outputType = outputType ?? HtmlToImageOutputType.jpeg;
+    this.options = options || { xvfb: false, xvfbArgs: undefined };
+  }
+
+  private command(): string {
+    if (this.options.xvfb) {
+      return `xvfb-run ${this.options.xvfbArgs || ""} node ${require.resolve(
+        "electron/cli.js"
+      )} --no-sandbox ${script} --target ${this.url.toString()} --output ${this.output} --selector ${this.selector} --output-image-type ${this.outputType}`;
+    }
+
+    return `node ${require.resolve(
+      "electron/cli.js"
+    )} --no-sandbox ${script} --target ${this.url.toString()} --output ${this.output} --selector ${this.selector} --output-image-type ${this.outputType}`;
   }
 
   async render(): Promise<OperationResult> {
-    // Render the Image
-    const command = `node ${require.resolve(
-      "electron/cli.js"
-    )} --no-sandbox ${script} --target ${this.url.toString()} --output ${this.output} --selector ${this.selector} --output-image-type ${this.outputType}`;
+    // Render the PDF
+    const command = this.command();
     const { stdout } = await execp(command);
 
     // Clean output
