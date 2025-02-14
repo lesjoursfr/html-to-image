7f1f94ff95d7b12ad76feb79e79e9654e95e91c5
Adrien ERAUD <e-adrien@users.noreply.github.com>
2025-02-14 10:04:49 +0100

    Fix CI

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,32 @@ function cleanOutput(std: string): OperationResult | null {
   return result;
 }
 
+function isExecpError(err: unknown): err is Error & { stdout: string; stderr: string } {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return err instanceof Error && typeof (err as any).stdout === "string" && typeof (err as any).stderr === "string";
+}
+
+async function exitCodeSafeExec(command: string): Promise<string> {
+  // Execute the comand
+  let result: { stdout: string; stderr: string };
+  try {
+    result = await execp(command);
+  } catch (execpErr) {
+    // Restore the result if there is an error
+    if (isExecpError(execpErr)) {
+      result = { stdout: execpErr.stdout, stderr: execpErr.stderr };
+    } else {
+      result = { stdout: "", stderr: execpErr instanceof Error ? execpErr.message : "Missing error message" };
+    }
+  }
+
+  if (result.stdout !== "") {
+    return result.stdout;
+  }
+
+  throw new Error(`Operation failed [command : ${command}]\n${result.stderr}`);
+}
+
 export class HtmlToImage {
   url: URL;
   output: string;
@@ -71,7 +97,7 @@ export class HtmlToImage {
   async render(): Promise<OperationResult> {
     // Render the Image
     const command = this.command();
-    const { stdout } = await execp(command);
+    const stdout = await exitCodeSafeExec(command);
 
     // Clean output
     const opResult = cleanOutput(stdout);
